1d40c763233054b938f559d8664f8e0ea30bee1b
Mohamedeissa72 <157132851+Mohamedeissa72@users.noreply.github.com>
2024-03-02 02:39:16 +0200

    Add files via upload

---
+++ Beginner_Level_Projects/Project_2/Ramp_Function.m
@@ -0,0 +1,16 @@
+%  Plot Ramp
+
+k = 2;
+
+R = 10e3;
+C = 1e-7;
+
+T = R*C;
+
+num = [0 k];
+den = [T 1];
+G = tf (num,den);
+t = 0:0.1:10;
+u_ramp = t;
+[y_ramp,t_ramp] = lsim (G,u_ramp,t);
+plot (t_ramp,y_ramp)
